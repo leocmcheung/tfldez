c8d49910b5ad6ea2569753854eceac0680dd7880
LeoC <leocmcheung@gmail.com>
2023-04-26 15:45:09 +0000

    updating calculation of duration post-202209

--- models/core/fact_most_bike.sql
+++ models/core/fact_most_bike.sql
@@ -1,3 +1,3 @@
-SELECT r.bike_id, COUNT(*) AS num_rentals, sum(r.duration) AS duration
+SELECT r.bike_id, COUNT(*) AS num_rentals, sum(r.duration)/3600 AS duration
 FROM tfl_bike_dwh.stg_bike_data AS r
 GROUP BY r.bike_id ORDER BY num_rentals DESC
